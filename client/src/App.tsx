b2d09122c59320926e94e5f7829fecf062511176
zmulji <znmulji1@gmail.com>
2020-10-14 12:30:34 -0400

    Added packages, updated App w/ basic theme & route to SearchPage, switched to absolute imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import SearchPage from "pages/SearchPage/SearchPage";
 
-function App() {
+const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: {
+            main: "#d52b1d"
+          },
+          secondary: {
+            main: "#ffbe3d"
+          },
+          success: {
+            main: "#008330"
+          },
+          error: {
+            main: "#ed7000"
+          },
+          info: {
+            main: "#0077b4"
+          },
+          warning: {
+            main: "#ffbc3d"
+          },
+        }
+      }),
+    [prefersDarkMode]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <SearchPage />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ client/src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchPage = () => {
+    return <>search page imported</>
+}
+
+export default SearchPage;
